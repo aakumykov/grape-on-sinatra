fd0854127115b870148b9672050e0e9a7041e29a
Андрей Кумыков <aakumykov@yandex.ru>
2016-09-05 14:51:25 +0700

    Начальный набросок

--- api.rb
+++ api.rb
@@ -1,56 +1,26 @@
 class API < Grape::API
 	format :json
 
-	helpers do
-		def print_params
-			puts "--------- params begin ---------"
-			params.each do |p|
-				key = p.first
-				value = p.last
-				puts "#{key} -> #{value} (#{value.class})"
-			end
-			puts "--------- params end ---------"
+	resource '/items' do
+
+		get '/' do
+			{ list:'items' }
 		end
-	end
 
-	before do
-		params.a = params.a.to_f
-		params.b = params.b.to_f
-		print_params
-	end
+		get '/:id' do
+			{ view: 'item' }
+		end
 
-	get :hello do
-    	{ hello: 'world', params: params }
-	end
+		post '/new' do
+			{ new: 'item' }
+		end
 
-	params do
-		requires :operation, type: String
-		requires :a, type: Numeric
-		requires :b, type: Numeric
-	end
-	get '/:operation' do
-		operation = params.operation.to_sym
-		a,b = params.a.to_f, params.b.to_f
-		{ operation => a.send(operation,b) }
-	end
+		get ':id/edit' do
+			{ edit: 'item' }
+		end
 
-	params do
-		requires :title, type: String, regexp: /^.+$/
-		requires :content, type: String
-		optional :comment, type: String, default: 'no comments'
-		optional :index, type: Integer, default: '1'
-	end
-	post '/items/new' do
-		result = {}
-
-		params.each do |p|
-			key = p.first
-			value = p.last
-
-			result[key.to_sym] = value
-			puts "#{key} -> #{value} (#{value.class})"
-
-			result
+		delete ':id/delete' do
+			{ delete: 'item' }
 		end
 	end
 end
